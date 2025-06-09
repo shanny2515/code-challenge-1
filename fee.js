09901ec24df0868c78fdfca7c384efd972aefc9e
shanny2515 <damarshania05@gmail.com>
2025-06-09 21:57:11 +0300

    Remove unnecessary condition check for transaction fee calculation

--- fee.js
+++ fee.js
@@ -4,7 +4,6 @@ process.stdin.setEncoding('utf8')
 process.stdin.on('data', (inpt) =>{
     let amountToSend = Number(inpt.trim())
     let transactionFee = (amountToSend * 0.015)
-    if  (Math.ceil(transactionFee)){
     if (transactionFee < 10){
         transactionFee = 10
     }else if (transactionFee > 70){
@@ -16,6 +15,6 @@ process.stdin.on('data', (inpt) =>{
     Total amount to be debited: KES ${totalAmount}
         
     Send Money Securely!`)
-    }
+    
     process.exit();
 })
