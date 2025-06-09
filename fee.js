5382dc955cd36bd4bf9cd7f50c7e4800e2dce150
shanny2515 <damarshania05@gmail.com>
2025-06-09 21:54:12 +0300

    Fix transaction fee calculation and improve output formatting

--- fee.js
+++ fee.js
@@ -3,22 +3,19 @@ console.log("Unatuma ngapi? KES")
 process.stdin.setEncoding('utf8')
 process.stdin.on('data', (inpt) =>{
     let amountToSend = Number(inpt.trim())
-    if  (Math.ceil(amountToSend)){
-    let transactionFee = (amountToSend * 0.15)
-    if (Math.ceil(transactionFee)){
+    let transactionFee = (amountToSend * 0.015)
+    if  (Math.ceil(transactionFee)){
     if (transactionFee < 10){
         transactionFee = 10
     }else if (transactionFee > 70){
         transactionFee = 70
-    }else {
-        transactionFee = transactionFee
     }
     let totalAmount =  amountToSend +  transactionFee
-    console.log (`sending KES ${amountToSend} 
-        Calculated Transaction Fee: KES ${transactionFee}
-        Total amount to be debited: KES ${totalAmount}
+    console.log (`    Sending KES ${amountToSend} 
+    Calculated Transaction Fee: KES ${transactionFee}
+    Total amount to be debited: KES ${totalAmount}
         
-        Send Money Securely!`)
-    }}
+    Send Money Securely!`)
+    }
     process.exit();
 })
